b94cc143a8595d562d4a53ee485d9049f91a97b2
hestisitaresmi <42725278+hestisitaresmi@users.noreply.github.com>
2018-09-24 10:58:19 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,23 @@
 <?php
+	echo "Daftar Hobi : "."<br>";
+	$hobby = array($_POST['hobi']);
+
+	foreach($_POST['hobi'] as $hobby) {
+
+	echo "-".$hobby."<br>" ;
+	}
+
+	echo "<br>"."Upload Foto : ";
+	
+	$file=$_FILES['dokumen'];
+	$nama_file=$file['name'];
+	$nama_tmp=$file['tmp_name'];
+	$upload_dir= "upload/";
+	move_uploaded_file($nama_tmp, $upload_dir.$nama_file);
+	
 
 ?>
+<br><img width = "500" src="upload/<?php echo $nama_file; ?>"><br>
+
+<br><a href="hal.html"><input type="submit" name="Delete" value="DELETE" ></a>
+
